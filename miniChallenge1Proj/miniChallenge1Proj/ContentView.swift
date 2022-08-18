1e0ad1e8f5da558cbbdf6a6f48e96325d1779f8b
MatheusZanivan <zanivanandrade@gmail.com>
2022-08-18 15:58:17 -0300

    Projeto Criado

---
+++ miniChallenge1Proj/miniChallenge1Proj/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  miniChallenge1Proj
+//
+//  Created by Matheus Zanivan on 18/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ miniChallenge1Proj/miniChallenge1Proj/miniChallenge1ProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  miniChallenge1ProjApp.swift
+//  miniChallenge1Proj
+//
+//  Created by Matheus Zanivan on 18/08/22.
+//
+
+import SwiftUI
+
+@main
+struct miniChallenge1ProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
