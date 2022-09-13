a2bd0224bc7c04588bd184b895f512cea660887f
Vinicius Gomes <78090044+vinicius-g-oliv@users.noreply.github.com>
2022-09-13 14:44:30 -0300

    Boat loucalizacao feito

---
+++ miniChallenge1Proj/miniChallenge1Proj/LocationManager.swift
@@ -0,0 +1,26 @@
+import MapKit
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    @Published var authorizationStatus: CLAuthorizationStatus
+    
+    private let locationManager: CLLocationManager
+    
+    override init() {
+        locationManager = CLLocationManager()
+        authorizationStatus = locationManager.authorizationStatus
+        
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+    }
+    func requestPermission() {
+        locationManager.requestWhenInUseAuthorization()
+    }
+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        authorizationStatus = manager.authorizationStatus
+    }
+}
+
