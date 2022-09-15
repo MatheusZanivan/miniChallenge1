b843dd96e8e1e4f043919d9a7984ee134df6aa2e
Higor <higorcris21@hotmail.com>
2022-09-15 14:43:40 -0300

    Localizacao_Final

--- miniChallenge1Proj/miniChallenge1Proj/ContentView.swift
+++ miniChallenge1Proj/miniChallenge1Proj/ContentView.swift
@@ -1,20 +1,52 @@
-//
-//  ContentView.swift
-//  miniChallenge1Proj
-//
-//  Created by Matheus Zanivan on 18/08/22.
-//
-
+import Combine
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    
+    @StateObject var deviceLocationService = DeviceLocationService.shared
+    
+    @State var tokens: Set<AnyCancellable> = []
+    @State var coordinates: (lat: Double, lon: Double) = (0,0)
+    
+    
+    
     var body: some View {
-        VStack {
-            Text("Pedrona")
+        ZStack {
+            Map(coordinateRegion: .constant(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: coordinates.lat, longitude: coordinates.lon), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))), showsUserLocation: true)
+                .ignoresSafeArea()
+                .tint(.pink)
+            }
+        .onAppear {
+            observeCoordinateUpdates()
+            observeLocationAccessDenied()
+            deviceLocationService.requestLocationUpdates()
         }
-        
-        
     }
+    
+    //Verifica se existe erro e Atualiza as coordenadas
+    func observeCoordinateUpdates(){
+        deviceLocationService.coordinatesPublisher
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                if case .failure(let error) = completion {
+                    print(error)
+                }
+            } receiveValue: { coordinates in
+                self.coordinates = (coordinates.latitude, coordinates.longitude)
+            }
+            .store(in: &tokens)
+    }
+    
+    func observeLocationAccessDenied() {
+        deviceLocationService.deniedLocationAcessPublisher
+            .receive(on: DispatchQueue.main)
+            .sink {
+                print("Show some kind of alert to the user")
+            }
+            .store(in: &tokens)
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
